c3033b3cebd81b6a1e23e143418da8ea7737abd7
MostafaHussien2000 <MostafaHussien2000@users.noreply.github.com>
2022-08-17 03:43:47 +0200

    version 1.0

---
+++ js/projects.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    id: 0,
+    name: "Tic Tac Toe!",
+    coverSrc: "./assets/projects/xo.png",
+    repo: "https://github.com/MostafaHussien2000/tic-tac-toe-game-multiplayer",
+    url: "https://x-vs-o-forever.netlify.app/",
+    tools: ["HTML", "CSS", "JS"],
+  },
+  {
+    id: 1,
+    name: "Breaking Bad Wiki!",
+    coverSrc: "./assets/projects/breaking-bad.png",
+    repo: "https://github.com/MostafaHussien2000/breaking-bad-react-project",
+    url: "https://breaking-bad-wiki-mh.netlify.app/",
+    tools: ["ReactJS", "JS"],
+  },
+  {
+    id: 2,
+    name: "Rock, Paper & Scissor!",
+    coverSrc: "./assets/projects/rps.png",
+    repo: "https://github.com/MostafaHussien2000/rcpgame",
+    url: "https://rock-paper-scissors-game-by-mostafa.netlify.app/",
+    tools: ["HTML", "CSS", "JS"],
+  },
+];
